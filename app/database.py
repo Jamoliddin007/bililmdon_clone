fe467e2e02f786b308b64c211ae191e00cf64bf9
jamoliddin007 <jamoliddin_007>
2025-04-04 17:42:55 +0500

    ✅ Refactor: fixed DeclarativeBase usage in database.py

--- app/database.py
+++ app/database.py
@@ -1,34 +1,26 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session,DeclarativeBase
+from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
 from typing import Annotated
 from fastapi import Depends
-
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-
 DB_NAME = os.getenv("DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_USER = os.getenv("DB_USER")
-DB_PASSWORD =os.getenv("DB_PASSWORD")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
+# ✅ To‘g‘ri deklaratsiya
 class Base(DeclarativeBase):
     pass
 
 SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-
-
-engine = create_engine( 
-    SQLALCHEMY_DATABASE_URL 
-)
-
-SessionLocal = sessionmaker(bind=engine) 
-
-Base = DeclarativeBase
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 def get_db():
     db = SessionLocal()
@@ -36,6 +28,5 @@ def get_db():
         yield db
     finally:
         db.close()
-
 
 db_dep = Annotated[Session, Depends(get_db)]
