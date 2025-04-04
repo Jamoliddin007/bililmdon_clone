306bf5822a694f0285c1b97765567440e56b4180
jamoliddin007 <jamoliddin_007>
2025-04-04 10:58:26 +0500

    fixed database field again

--- app/database.py
+++ app/database.py
@@ -13,7 +13,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_USER = os.getenv("DB_USER")
-DB_PASSWORD =os.getenv("DB_NAME")
+DB_PASSWORD =os.getenv("DB_PASSWORD")
 
 class Base(DeclarativeBase):
     pass
