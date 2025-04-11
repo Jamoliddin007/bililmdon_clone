1547c1c76be96d8b281770cc6b81054e5913042f
jamoliddin007 <jamoliddin_007>
2025-04-11 22:00:02 +0500

    fix: prevent duplicated tags in Swagger UI

--- app/routers/question.py
+++ app/routers/question.py
@@ -5,10 +5,10 @@ from app.dependencies import db_dep
 from app import models
 from app.schemas import question as schemas
 
-router = APIRouter(prefix="/questions", tags=["Questions"])
+router = APIRouter(prefix="/questions")
 
 # These endpoints are for topics
-@router.post("/topics", response_model=schemas.TopicRead)
+@router.post("/topics", response_model=schemas.TopicRead, tags=["Topics"])
 def create_topic(topic: schemas.TopicCreate, db: db_dep):
     topic_obj = models.Topic(**topic.model_dump())
     db.add(topic_obj)
@@ -17,7 +17,7 @@ def create_topic(topic: schemas.TopicCreate, db: db_dep):
     return topic_obj
 
 
-@router.delete("/topics/{topic_id}")
+@router.delete("/topics/{topic_id}", tags=["Topics"])
 def delete_topic(topic_id: int, db: db_dep):
     topic = db.query(models.Topic).get(topic_id)
     if not topic:
@@ -28,7 +28,7 @@ def delete_topic(topic_id: int, db: db_dep):
 
 
 # These endpoints are for question
-@router.post("/", response_model=schemas.QuestionRead)
+@router.post("/", response_model=schemas.QuestionRead, tags=["Questions"])
 def create_question(question: schemas.QuestionCreate, db: db_dep):
     question_obj = models.Question(**question.model_dump())
     db.add(question_obj)
@@ -37,12 +37,12 @@ def create_question(question: schemas.QuestionCreate, db: db_dep):
     return question_obj
 
 
-@router.get("/", response_model=List[schemas.QuestionRead])
+@router.get("/", response_model=List[schemas.QuestionRead], tags=["Questions"])
 def list_questions(db: db_dep):
     return db.query(models.Question).all()
 
 
-@router.delete("/{question_id}")
+@router.delete("/{question_id}", tags=["Questions"])
 def delete_question(question_id: int, db: db_dep):
     question = db.query(models.Question).get(question_id)
     if not question:
@@ -53,7 +53,7 @@ def delete_question(question_id: int, db: db_dep):
 
 
 # These endpoints are for option
-@router.post("/options", response_model=schemas.OptionRead)
+@router.post("/options", response_model=schemas.OptionRead, tags=["Options"])
 def create_option(option: schemas.OptionCreate, db: db_dep):
     option_obj = models.Option(**option.model_dump())
     db.add(option_obj)
@@ -62,12 +62,12 @@ def create_option(option: schemas.OptionCreate, db: db_dep):
     return option_obj
 
 
-@router.get("/options", response_model=List[schemas.OptionRead])
+@router.get("/options", response_model=List[schemas.OptionRead], tags=["Options"])
 def list_options(db: db_dep):
     return db.query(models.Option).all()
 
 
-@router.delete("/options/{option_id}")
+@router.delete("/options/{option_id}", tags=["Options"])
 def delete_option(option_id: int, db: db_dep):
     option = db.query(models.Option).get(option_id)
     if not option:
