d4f9942ac0a15a7c4501b6f24ef34b8b4f2753a9
jamoliddin007 <jamoliddin_007>
2025-04-12 12:30:08 +0500

    feat: add Question model and endpoints

--- app/schemas/question.py
+++ app/schemas/question.py
@@ -1,13 +1,21 @@
 from pydantic import BaseModel
+from typing import Optional, List
+from datetime import datetime
 class QuestionCreate(BaseModel):
     title: str
-    description: str
+    description: Optional[str] = None
     topic_id: int
+    owner_id: int
 
-class QuestionRead(QuestionCreate):
+
+class QuestionRead(BaseModel):
     id: int
-    created_at: str
-    updated_at: str
+    title: str
+    description: Optional[str] = None
+    topic_id: int
+    owner_id: int
+    created_at: datetime
+    updated_at: datetime
 
     class Config:
-        orm_mode = True
+        from_attributes = True
