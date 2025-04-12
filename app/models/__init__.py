76c795cb0bdb8114db35b2b996157b9532d1bd47
jamoliddin007 <jamoliddin_007>
2025-04-12 11:47:57 +0500

    fix: optimize model imports using __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+# app/models/__init__.py
+
+from .user import User
+from .topic import Topic
+from .game import Game
+from .question import Question
+from .option import Option
+from .participation import Participation
+from .submission import Submission
+from .game_question import GameQuestion
