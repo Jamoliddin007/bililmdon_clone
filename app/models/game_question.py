5d5021d252a6abee35d04908d6ddf02dfac084fa
jamoliddin007 <jamoliddin_007>
2025-04-12 11:49:20 +0500

    feat: add GameQuestion model

---
+++ app/models/game_question.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy import Integer, ForeignKey
+from app.models import Game, Question
+
+
+from app.database import Base
+
+class GameQuestion(Base):
+    __tablename__ = "game_questions"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    game_id: Mapped[int] = mapped_column(Integer, ForeignKey("games.id"))
+    question_id: Mapped[int] = mapped_column(Integer, ForeignKey("questions.id"))
+
+    question: Mapped["Question"] = relationship(back_populates="games")
+    game: Mapped["Game"] = relationship(back_populates="questions")
